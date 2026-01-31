4e455cedc564c01767a998adfbace09faf50e0b3
shivaraj110 <shivarajchandaragi9@gmail.com>
2026-02-01 00:48:35 +0530

    did bubble sort with mannual swapping to avoid borrow checker

---
+++ src/bubble_sort.rs
@@ -0,0 +1,11 @@
+pub fn bubble_sort(arr: &mut [i32; 9]) {
+    for i in 0..arr.len() {
+        for j in 0..arr.len() - i - 1 {
+            if arr[j] > arr[j + 1] {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
