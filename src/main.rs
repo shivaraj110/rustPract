12028cc789683e9533fd0da2d36a1e00c82d0ea3
shivaraj110 <shivarajchandaragi9@gmail.com>
2026-02-01 00:48:53 +0530

    use bubble_sort crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use crate::bubble_sort::bubble_sort;
+mod bubble_sort;
 mod string;
-
 fn main() {
     string::string_match();
+    let mut array: [i32; 9] = [23, 43, 56, 78, 11, 14, 16, 65, 34];
+    bubble_sort(&mut array);
+    println!("Sorted array: {:?}", array);
 }
