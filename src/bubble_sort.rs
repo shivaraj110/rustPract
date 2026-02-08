9441dab5179b4f0cb029da8670eb82bc9e75fb23
shivaraj110 <shivarajchandaragi9@gmail.com>
2026-02-08 22:22:13 +0530

    also bubble_sort but with while loops

--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -9,3 +9,20 @@ pub fn bubble_sort(arr: &mut [i32; 9]) {
         }
     }
 }
+
+//Also bubble_sort but using while loops
+pub fn bubble_sort_while(arr: &mut [i32; 9]) {
+    let mut i = 0;
+    while i < arr.len() {
+        let mut j = 0;
+        while j < arr.len() - i - 1 {
+            if arr[j] > arr[j + 1] {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use crate::bubble_sort::bubble_sort;
+use crate::bubble_sort::bubble_sort_while;
 mod bubble_sort;
 mod string;
 fn main() {
     string::string_match();
     let mut array: [i32; 9] = [23, 43, 56, 78, 11, 14, 16, 65, 34];
     bubble_sort(&mut array);
-    println!("Sorted array: {:?}", array);
+    println!("Sorted array: (For loop) {:?}", array);
+    array = [23, 43, 56, 78, 11, 14, 16, 69, 34];
+    bubble_sort_while(&mut array);
+    println!("Sorted array (While loop): {:?}", array);
 }
